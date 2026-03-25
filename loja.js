d44c6c8efbf25947d9e4ebd91559e24f917a3a24
trajano777 <gabriel.gregzigouski@escola.pr.gov.br>
2026-03-25 11:37:13 +0000

    loja

---
+++ loja.js
@@ -0,0 +1,30 @@
+class produto {
+    constructor(nome, preco){
+        this.nome = nome
+        this.preco = preco
+        }
+}
+
+class carrinho{
+    constructor(){
+        this.produtos = []
+    }
+
+    adicionarproduto(produto){
+        this.produto.push(produto)
+    }
+
+}
+
+class pagamento{
+
+}
+
+class pedido{
+
+}
+
+class cliente{
+
+}
+
